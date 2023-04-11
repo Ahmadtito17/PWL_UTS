dac2cb6c18877c5e7df71a9448a9c1dcd3d3c2b9
= Rizky Angkata <= rizmadoris@gmail.com>
2023-04-11 20:59:43 +0700

    add game

---
+++ database/migrations/2023_04_11_134838_create_games_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('game', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_game', 10)->unique();
+            $table->string('nama_game', 30)->nullable();
+            $table->string('creator_game', 30)->nullable();
+            $table->string('harga_game', 10)->nullable();
+            $table->date('tahun_rilis')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+};
