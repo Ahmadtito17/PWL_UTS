0359e9a25abed310972176776dcad401d3e0cccb
= Rizky Angkata <= rizmadoris@gmail.com>
2023-04-11 21:06:12 +0700

    add game

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\GameController;
 use App\Http\Controllers\LaguController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,3 +23,4 @@ Route::get('/', function () {
 Route::get('/dashboard', [DashboardController::class, 'index']);
 Route::get('/', [DashboardController::class, 'index']);
 Route::resource('/lagu', LaguController::class)->parameter('lagu', 'id');
+Route::resource('/game', GameController::class)->parameter('game', 'id');
